2f10061ca7ffc78d1897854136532c312d0a3859
jaff521 <jaff521@163.com>
2025-02-18 18:40:03 +0800

    工业互联网AI助理，后台API是腾讯云dify

--- config/index.ts
+++ config/index.ts
@@ -3,15 +3,15 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
-  privacy_policy: '',
-  default_language: 'en',
+  title: 'II_AI',           // 应用标题
+  description: '',          // 应用描述
+  copyright: '',           // 版权信息
+  privacy_policy: '',      // 隐私政策链接
+  default_language: 'zh-Hans' // 默认语言设置为简体中文
 }
 
-export const isShowPrompt = false
-export const promptTemplate = 'I want you to act as a javascript console.'
+export const isShowPrompt = true
+export const promptTemplate = ''
 
 export const API_PREFIX = '/api'
 
